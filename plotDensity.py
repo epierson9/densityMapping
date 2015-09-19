71c19d633dde226b408e0c7080ac958724885bbf
Emma Pierson <epierson@Emmas-MacBook-Pro-2.local>
2015-09-18 18:16:52 -0700

    Added kernel density estimation and more complete comments

--- plotDensity.py
+++ plotDensity.py
@@ -1,10 +1,12 @@
+from pylab import *
 from sklearn.neighbors import KNeighborsClassifier 
+from sklearn.neighbors.kde import KernelDensity
 from matplotlib import path
 import random
 from mpl_toolkits.basemap import Basemap 
 import numpy as np
 import pandas as pd
-from pylab import *
+
 
 def pnpoly(x, y, xyverts):
     """
@@ -49,44 +51,84 @@ def points_in_polys(points, polys):
             mask = mask | points_inside_poly(points, poly)
     return np.array(mask)
 
-def makeNearestNeighborsDensityPlot(filename, col_of_interest, title_string = None, min_lat = 15, max_lat = 50, min_long = -130, max_long = -65, maskOffWater = True, cmap = 'bwr', color_min = 0, color_max = 1, n_neighbors = 5, center_around_america = True, res = .2):
-    d = pd.read_csv(filename)
-    geolocated = d.dropna(subset = ['lat', 'lon', col_of_interest]) 
-    idxs = (geolocated['lat'] > min_lat) & (geolocated['lat'] < max_lat) 
-    idxs = idxs &  (geolocated['lon'] > min_long) & (geolocated['lon'] < max_long) 
-    geolocated = geolocated.loc[idxs]
-    model = KNeighborsClassifier(n_neighbors = n_neighbors)
-    print 'Total number of points', len(geolocated), 'in column of interest nonzero', geolocated[col_of_interest].sum()
-    model.fit(geolocated[['lat', 'lon']], geolocated[col_of_interest])
+def makeNearestNeighborsDensityPlot(filename, col_of_interest = None, title_string = None, min_lat = 15, max_lat = 50, min_lon = -130, max_lon = -65, maskOffWater = True, cmap = 'bwr', color_min = None, color_max = None, n_neighbors = 20, res = .2, weights = lambda x:np.exp(-(x ** 2) / 3 ** 2)):
     
+    """
+    Makes a map of the density of events. Red = higher density, blue = lower density. 
+    filename: the name of the file that contains the data. See sample_data.csv for an example. Should contain at least two columns: lat (latitude) and lon (longitude). 
+    col_of_interest: If None, uses kernel density estimation to just plot the overall density of events. If not None, col_of_interest should be a column that contains 0s and 1s, where 1 denotes events of interest and 0 denotes background events; then uses k-nearest neighbors to estimate the fraction of events which are 1s. 
+    title_string: The title for the map. 
+    min_lat, max_lat, min_long, max_long: the boundaries of the map, in degrees. Negative longitudes = west; negative latitudes = south. By default set to borders of North America. 
+    maskOffWater: if True, does not map for water. True by default. 
+    cmap: the name of the colormap to use. See http://matplotlib.org/examples/color/colormaps_reference.html for list of options. 
+    color_min, color_max: which densities are mapped to the maximum and minimum colors. For k-nearest neighbors, set to 0 and 1 by default; for kernel density estimation, set to 0 and the maximum density estimate by default. May have to play with these to get informative maps. 
+    n_neighbors: for k-nearest neighbors, the number of neighbors to use to compute densities. Try increasing if the map is very spiky. 
+    res: how high-resolution the map is. Lower numbers = HIGHER resolution (but will take longer to make.)
+    weights: for k-nearest neighbors, how to weight the points as a function of distance. By default uses a Gaussian kernel. Don't play with this unless you know what you're doing. 
+    """
+    d = pd.read_csv(filename)
+    if not (('lat' in d.columns) and ('lon' in d.columns)):
+        raise Exception('Error: dataset must contain lat and lon')
+    
+    if (col_of_interest is not None) and (col_of_interest not in d.columns):
+        raise Exception('Error: dataset must contain column labeled %s' % col_of_interest)
+        if not set(d[col_of_interest]) == set([0, 1]):
+            raise Exception("Error: column %s must contain 0s and 1s" % col_of_interest)
+    #Filter for events with locations. 
+    geolocated = d.dropna(subset = ['lat', 'lon']) 
+    idxs = (geolocated['lat'] > min_lat) & (geolocated['lat'] < max_lat) 
+    idxs = idxs &  (geolocated['lon'] > min_lon) & (geolocated['lon'] < max_lon) 
+    geolocated = geolocated.loc[idxs]
+    
+    #Fit the appropriate model: k-nearest neighbors if col_of_interest is not None, otherwise Kernel Density Estimation. 
+    if col_of_interest is not None:
+        model = KNeighborsClassifier(n_neighbors = n_neighbors, weights = weights)
+        print 'Total number of points', len(geolocated), 'in column of interest nonzero', geolocated[col_of_interest].sum()
+        model.fit(geolocated[['lat', 'lon']], geolocated[col_of_interest])   
+    else:
+        print 'Total number of points', len(geolocated)
+        model = KernelDensity(kernel='gaussian', bandwidth = 3).fit(geolocated[['lat', 'lon']])
+    
+    #Create a grid of points at which to predict. 
     x = np.arange(min_lat, max_lat, res)
-    y = np.arange(min_long, max_long, res)
+    y = np.arange(min_lon, max_lon, res)
     X, Y = meshgrid(x, y)
     numel = len(X) * len(X[0, :])
     Z = np.zeros(X.shape)
     unraveled_x = X.reshape([numel, 1])
     unraveled_y = Y.reshape([numel, 1])
-    figure(figsize = [20, 10])
-    m = Basemap(llcrnrlat=min_lat, urcrnrlat=max_lat, llcrnrlon=min_long, urcrnrlon=max_long, resolution='l', fix_aspect = False)
     data_to_eval = np.hstack([unraveled_x, unraveled_y])
     
+    #Make predictions using appropriate model. 
+    if col_of_interest is not None:
+        density = model.predict_proba(data_to_eval)[:, 1]
+        if color_min is None:
+            color_min = 0
+        if color_max is None:
+            color_max = 1
+    else:
+        density = np.exp(model.score_samples(data_to_eval))
+        if color_min is None:
+            color_min = 0
+        if color_max is None:
+            color_max = density.max()
+
+    #Make map. 
+    figure(figsize = [20, 10])    
+    m = Basemap(llcrnrlat = min_lat, urcrnrlat = max_lat, llcrnrlon = min_lon, urcrnrlon=max_lon, resolution='l', fix_aspect = False)
     if maskOffWater:
         m.drawcoastlines()
         x, y = m(data_to_eval[:,1], data_to_eval[:,0])
         loc = np.c_[x, y]
         polys = [p.boundary for p in m.landpolygons]
         on_land = points_in_polys(loc, polys) 
-        density = model.predict_proba(data_to_eval)[:, 1]
-        max_density = density.max()
-        density[~on_land] = max_density / 2
+        density[~on_land] = (color_min + color_max) / 2
     
     density = density.reshape(X.shape)
-    cset1 = contourf(Y, X, density, levels = np.linspace(color_min, color_max, 25))
-    
+    contourf(Y, X, density, levels = np.linspace(color_min, color_max, 25))
     m.drawcoastlines(linewidth = 2)
     m.drawcountries(linewidth = 2)
     m.drawstates(linewidth = 2)
-    
     colorbar()
     set_cmap(cmap)
     if title_string is not None:
@@ -95,7 +137,11 @@ def makeNearestNeighborsDensityPlot(filename, col_of_interest, title_string = No
     else:
         savefig('plot.png', dpi = 300, format = 'png')
     show()
+
 def generateSimulatedData():
+    """
+    This generates some (not very realistic) simulated data.
+    """
     lats = list(35 + (np.random.random([2000,]) - .5) * 10)
     longs = list(-100 + (np.random.random([1000,]) - .5) * 20) + list(-80 + (np.random.random([1000,]) - .5) * 20)
     is_pizza_shop = [1 for i in range(1000)] + [0 for i in range(1000)] 
@@ -105,7 +151,9 @@ def generateSimulatedData():
 if __name__ == '__main__':
     generateSimulatedData()
     filename = 'sample_data.csv'
-    col_of_interest = 'pizza_shops'
-    makeNearestNeighborsDensityPlot(filename, col_of_interest)
+    print "Making plot without normalizing!"
+    makeNearestNeighborsDensityPlot(filename, col_of_interest = None, title_string = "Overall Shop Density")
+    print "Making plot with normalizing!"
+    makeNearestNeighborsDensityPlot(filename, col_of_interest = "pizza_shops", title_string = "Fraction of Shops which are Pizza Shops")
 
 
